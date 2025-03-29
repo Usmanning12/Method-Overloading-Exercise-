1bf21bfe82931a99488f238a3ba02923e2817245
Darron Manning <darronmanning@yahoo.com>
2025-03-29 12:24:19 -0500

    Completed MethodOverloading Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,47 @@
 {
     public class Program
     {
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+            
+        }
+
+        public static string Add(int NumOne, int NumTwo, bool isMoney)
+        {
+            var sum = NumOne + NumTwo;
+            if (isMoney && sum > 1)
+            {
+                return $" {sum} dollars";
+            }
+
+            else if (isMoney && sum == 1)
+            {
+                return $" {sum} dollar";
+                
+            }
+
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(20, 15, true));
         }
+
+
     }
 }
